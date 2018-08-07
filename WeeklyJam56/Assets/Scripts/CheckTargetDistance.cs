06b2793b02f94cfcce9d1f2dec9405db70cb9e0a
LKZsaldanha <38111193+LKZsaldanha@users.noreply.github.com>
2018-08-07 11:38:32 -0300

    Script de distancia

---
+++ WeeklyJam56/Assets/Scripts/CheckTargetDistance.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class CheckTargetDistance : MonoBehaviour {
+
+    public Transform target;
+    public float alertDistance;
+    public float limitDistance;
+    public float currentDistance;
+
+    private void Update()
+    {
+        currentDistance = Vector2.Distance(transform.position, target.position);
+                
+        if(currentDistance > alertDistance)
+        {
+            if(currentDistance > limitDistance)
+            {
+                DangerVisual();
+            }
+            else
+            {
+                AlertVisual();
+            }
+        }
+        else
+        {
+            NormalVisual();
+        }
+    }
+
+
+    private void NormalVisual()
+    {
+        Color tmp = GetComponent<SpriteRenderer>().color = Color.green;
+        tmp.a = .6f;
+        GetComponent<SpriteRenderer>().color = tmp;
+    }
+
+    private void AlertVisual()
+    {
+        Color tmp = GetComponent<SpriteRenderer>().color = Color.yellow;
+        tmp.a = .6f;
+        GetComponent<SpriteRenderer>().color = tmp;
+    }
+
+    private void DangerVisual()
+    {
+        Color tmp = GetComponent<SpriteRenderer>().color = Color.red;
+        tmp.a = .6f;
+        GetComponent<SpriteRenderer>().color = tmp;
+    }
+}
