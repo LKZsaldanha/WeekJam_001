86896ffec48fecddc3819a38716dbc31e0f03d00
LKZsaldanha <38111193+LKZsaldanha@users.noreply.github.com>
2018-08-07 11:38:16 -0300

    Scripts de movimentação

---
+++ WeeklyJam56/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    private float horizontalMove;
+    private bool jump = false;
+
+    private void Update ()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal")* runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime ,false, jump);
+        jump = false;
+    }
+}
---
+++ WeeklyJam56/Assets/Scripts/npc_Movement_Platformer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class npc_Movement_Platformer : MonoBehaviour {
+
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    private float horizontalMove;
+    private bool jump = false;
+
+    private void Update ()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal2")* runSpeed;
+
+        if (Input.GetButtonDown("Jump2"))
+        {
+            jump = true;
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime ,false, jump);
+        jump = false;
+    }
+}
